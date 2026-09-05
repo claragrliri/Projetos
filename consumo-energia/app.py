1071f03ee6b756013bc3d23153555ca4d21d9cab
Clara Graciano <clara.grliri@gmail.com>
2026-09-05 13:38:56 -0300

    Fix typos in app.py for appliance consumption calculation
    Corrected typos in variable names and print statements.

--- consumo-energia/app.py
+++ consumo-energia/app.py
@@ -1,11 +1,10 @@
-aparelho = input("Digite o nome do seu aparelho (ex.:Geladeira): ")
-
-potencia = int(input(f"Digite a potencia do(a) {aparelho} em watts (W): "))
-
-horasDia = float(input(f"DItige o tempo médio de uso diário do(a) {aparelho} em horas: "))
-
+aparelho = input("Digite o nome do seu aparelho (ex:.Geladeira): ")
+potencia = int(input(f"Digite a potêcia do(a) {aparelho} em watts (W):"))
+horasDia = float(input(f"Digite o tempo médio de uso diário do(a) {aparelho} em horas: "))
 consumoMensal = (potencia * horasDia * 30) / 1000
-
-print("-" * 40)
-print(f"O consumo mensal do(a) {aparelho} é de: {consumoMensal:.2f} kWh")
-print("-" * 40)
+custoEstimado = (consumoMensal * 0.75)
+ 
+print("-" * 50)
+print(f"Oconsumo mensal do (a) {aparelho} é de: {consumoMensal:.2f} kWh")
+print(f"O custo estimado mensal é de: R$ {custoEstimado:.2f}")
+print("-" * 50)
