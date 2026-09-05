20d9e72c46fae79b4f77a2b50e8c3004c8379e73
Clara Graciano <clara.grliri@gmail.com>
2026-09-05 11:30:37 -0300

    Add energy consumption calculation for appliances

--- consumo-energia/app.py
+++ consumo-energia/app.py
@@ -1,1 +1,11 @@
-# Projetos-consumo-energia-app.py
+aparelho = input("Digite o nome do seu aparelho (ex.:Geladeira): ")
+
+potencia = int(input(f"Digite a potencia do(a) {aparelho} em watts (W): "))
+
+horasDia = float(input(f"DItige o tempo médio de uso diário do(a) {aparelho} em horas: "))
+
+consumoMensal = (potencia * horasDia * 30) / 1000
+
+print("-" * 40)
+print(f"O consumo mensal do(a) {aparelho} é de: {consumoMensal:.2f} kWh")
+print("-" * 40)
